e49752f2803f0378d92ffdf19488a5dc691d1665
Corné de Jong <corne.dejong@me.com>
2018-02-26 23:04:45 +0100

    Initial Commit (0.1)

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'class.CodeNameGenerator.php';
+
+/* Get a new CodeName Generator */
+$generate = new CJ\CodeNameGenerator;
+/* Check if there are any specifications given */
+if(isset($_GET['atribute']) && !empty($_GET['atribute']))   $generate->givenAtribute = $_GET['atribute'];
+if(isset($_GET['object']) && !empty($_GET['object']))       $generate->givenObject = $_GET['object'];
+
+/* Get the codename */
+$generate->newCodeName();
+
+$generate->responde("JSON", $response);
+
+print($response);
+
+?>
