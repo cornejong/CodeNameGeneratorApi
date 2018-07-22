1da8d59861a0490392735eaf0b5d901bb2dd0c66
Corné de Jong <corne.dejong@me.com>
2018-07-22 18:04:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once 'class.CodeNameGenerator.php';
 /* Get a new CodeName Generator */
 $generate = new Generator\codeName;
 /* Check if there are any specifications given */
-if(isset($_GET['atribute']) && !empty($_GET['atribute']))   $generate->givenAtribute = $_GET['atribute'];
-if(isset($_GET['object']) && !empty($_GET['object']))       $generate->givenObject = $_GET['object'];
+if(isset($_GET['atribute']) && !empty($_GET['atribute']))   $generate->setAtribute = $_GET['atribute'];
+if(isset($_GET['object']) && !empty($_GET['object']))       $generate->setObject = $_GET['object'];
 
 /* Get the codename */
 $generate->newCodeName();
